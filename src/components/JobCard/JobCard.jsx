2e315ff1c51e5466c3b39a65fc1c8e0b9a32958b
mouradeljayi <mouradeljayi98@gmail.com>
2024-05-04 21:38:28 +0800

    setup filter component

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -1,21 +1,32 @@
 import './JobCard.css'
+import { CiHeart } from "react-icons/ci";
+import { IoMdHeart } from "react-icons/io";
+
+
 
 const JobCard = () => {
     return (
-       
-        <div className='custom-card'>
-            <img 
-                src="https://images.jobfinder.lu/insecure/rs:fit:640:640:1/ex:1/czM6Ly9qb2JmaW5kZXIucHVibGljLzI1YzFlMzI4ZjU4MzhkNGY4NjQyMzNlMmM1YzYwNGUxMzcxODVjYTA=" 
-                alt='Job Image' />
-            <div className='card-content'>
-                <h4>Ingénieur d'étude et développement</h4>
-                <p>ION Group</p>
-                <p>Contrat : CDI</p>
 
-                <p>Publié le : 30/04/2024</p>
-                
+        <div className='custom-card'>
+        <div className='grid-container'>
+          <div className='image-content'>
+            <img
+              src="https://images.jobfinder.lu/insecure/rs:fit:640:640:1/ex:1/czM6Ly9qb2JmaW5kZXIucHVibGljLzI1YzFlMzI4ZjU4MzhkNGY4NjQyMzNlMmM1YzYwNGUxMzcxODVjYTA="
+              alt='Job Image' />
+            <div className='card-content'>
+              <h4>Ingénieur Java Spring boot FullStack (F/H)</h4>
+              <p>ION Group</p>
+              <p>Contrat : CDI</p>
+              <p>Publié le : 30/04/2024</p>
             </div>
+          </div>
+          <div className='icon-container'>
+            <CiHeart />
+            {/* <IoMdHeart/> */}
+          </div>
         </div>
+      </div>
+      
     );
 };
 
--- src/views/JobsListing/JobsListing.jsx
+++ src/views/JobsListing/JobsListing.jsx
@@ -4,10 +4,13 @@ import CustomSelect from '../../components/common/CustomSelect/CustomSelect';
 import { CiSearch, CiFilter } from "react-icons/ci";
 import JobCard from '../../components/JobCard/JobCard';
 import Pagination from '../../components/common/Pagination/Pagination';
+import FilterJobs from '../../components/FilterJobs/FilterJobs';
 
 function JobsListing() {
 
   const [selectedNumber, setSelectedNumber] = useState(null);
+  const [isFilterOpen, setIsFilterOpen] = useState(false); // State to track filter visibility
+
 
   const handleNumberSelect = (number) => {
     setSelectedNumber(number);
@@ -20,14 +23,19 @@ function JobsListing() {
     // Fetch data for the new page here
   };
 
+  const toggleFilter = () => {
+    setIsFilterOpen(!isFilterOpen);
+  };
+
 
 
   return (
-    <div className='container'>
+    <div className='container-fluid '>
+      {isFilterOpen && <FilterJobs isFilterOpen={isFilterOpen} onClose={toggleFilter}/>}
       <h3>Nos offres d'emploi</h3>
       <section className='filter-container mt-20'>
         <div>
-          <button className='filter-btn'><CiFilter className='icon'/> Filtrer</button>
+          <button className='filter-btn' onClick={toggleFilter}><CiFilter className='icon'/> Filtrer</button>
         </div>
         <div>
           <input type="text" placeholder="Rechercher par titre, type, salaire..." className="jobs-search" />
