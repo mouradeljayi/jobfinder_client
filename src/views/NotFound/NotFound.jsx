9ec02e125ca7e45ff45628e51e67ad315af46b84
mouradeljayi <mouradeljayi98@gmail.com>
2024-06-01 19:48:46 +0100

    design not found page

--- src/views/NotFound/NotFound.jsx
+++ src/views/NotFound/NotFound.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import "./NotFound.css"
+import { useNavigate } from 'react-router-dom';
 
-function NotFound() {
-  return <h1>404 - Not Found</h1>;
+const NotFound = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div className="not-found-container">
+      <div className="content">
+        <h1 className="not-found-title">404 - Page Introuvable</h1>
+        <p className="not-found-message">Oops! La page que vous recherchez n'existe pas.</p>
+        <button className="go-back-button" onClick={goBack}>Retourner</button>
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
